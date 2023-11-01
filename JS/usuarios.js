f9c27e87396681eec99ccdd7a207268b24a7d4ef
Alessandro Grassini <119017437+AleOficial58@users.noreply.github.com>
2023-10-31 23:02:24 -0300

    Fix/Bug

--- JS/usuarios.js
+++ JS/usuarios.js
@@ -1,17 +1,17 @@
 const DB_USUARIOS = [
     {   id: 1,
-        username: "miguel",
-        password: "1234",
+        username: "Miguel",
+        password: "JUAN316",
         first_name: "Miguel Angel",
-        last_name: "Gonzalez",
-        email: "miguel@gmail.com"
+        last_name: "Borgo",
+        email: ""
     },
     
     {   id: 2,
-        username: "alan",
-        password: "1234",
+        username: "Alan",
+        password: "SALMOS234",
         first_name: "Alan",
-        last_name: "Borgo",
+        last_name: "Benjamin Borgo",
         email: "miguel@gmail.com"
     }
 
